6cf50f4f76b5ee266bb235e90089cf4b004bc554
kamogelo mogasoa <120066012+Kamogelo-7@users.noreply.github.com>
2024-09-15 12:09:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 //*Job probability game
 
 document.querySelector("#submit").addEventListener("click", function () {
-  let userNames = document.querySelector("#txt").value; //Stores the users name
-  let randomNumber = Math.floor(Math.random() * 100) + 1; //Generates a random number from 1 to 100
+  let userNames = document.querySelector("#txt").value; // Stores the user's name
+  let randomNumber = Math.floor(Math.random() * 100) + 1; // Generates a random number from 1 to 100
 
   setTimeout(function () {
-    document.querySelector("#txt").innerText = "";
+    document.querySelector("#txt").value = ""; // Clears the input field after 100ms
   }, 100);
 
   if (randomNumber > 70) {
     document.querySelector("p").innerText = "Wow you should start applying!";
-    document.querySelector("h3").innerText= randomNumber + "%";
+    document.querySelector("h3").innerText = randomNumber + "%";
     document.querySelector("h2").innerText = "Hey " + userNames + " you got";
   } else if (randomNumber >= 40 && randomNumber <= 70) {
     document.querySelector("p").innerText = "You are able to get a job!";
     document.querySelector("h3").innerText = randomNumber + "%";
-    document.querySelector("h2").innerHTML = "Hey " + userNames + " you got";
-  }
-  if (randomNumber < 30) {
-    document.querySelector("p").innerHTML =
+    document.querySelector("h2").innerText = "Hey " + userNames + " you got";
+  } else if (randomNumber < 30) {
+    document.querySelector("p").innerText =
       "YOOO WATCH OUT, YOUR COOKED NGL JUST CRY!";
-    document.querySelector("h3").innerHTML = randomNumber + "%";
-    document.querySelector("h2").innerHTML =
+    document.querySelector("h3").innerText = randomNumber + "%";
+    document.querySelector("h2").innerText =
       "Hey " + userNames + " no luck you got";
-    document.querySelector("h4").innerHTML = "Sorry try again ;)";
+    document.querySelector("h4").innerText = "Sorry try again ;)";
   }
+});
