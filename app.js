d3be4b063df9a17ea43d63b22731bf0bde6ffc15
kamogelo mogasoa <120066012+Kamogelo-7@users.noreply.github.com>
2024-09-15 11:57:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
 //*Job probability game
 
-$("#submit").on("click",function (){
-  let userNames = $("#txt").val(); //Stores the users name
+function jobProbabilityGame() {
+document.querySelector("#submit").addEventListener("click", function () {
+  let userNames = document.querySelector("#txt").value; //Stores the users name
 
   let randomNumber = Math.floor(Math.random() * 100) + 1; //Generates a random number from 1 to 100
+  let userNames = prompt("Enter your name"); //Stores the users name
 
   if (randomNumber > 70) {
-    $("p").text("😀 Wow you should start applying!");
-    $("h3").text(`${randomNumber}%`);
-    $("h2").text(`Hey ${userNames}, you got`);
-  } 
-  else if (randomNumber >= 40 && randomNumber <= 70) {
-    $("p").text("You are able to get a job!");
-    $("h3").text(`${randomNumber}%`);
-    $("h2").text(`Hey ${userNames}, you got`);
+    document.querySelector("p").innerHTML = "Wow you should start applying!";
+    document.querySelector("h3").innerHTML = randomNumber + "%";
+    document.querySelector("h2").innerHTML = "Hey " + userNames + " you got";
+    document.querySelector("p").innerText = "😀 Wow you should start applying!";
+    document.querySelector("h3").innerText = randomNumber + "%";
+    document.querySelector("h2").innerText = "Hey " + userNames + " you got";
+  } else if (randomNumber >= 40 && randomNumber <= 70) {
+    document.querySelector("p").innerHTML = "You are able to get a job!";
+    document.querySelector("h3").innerHTML = randomNumber + "%";
+    document.querySelector("h2").innerHTML = "Hey " + userNames + " you got";
+    document.querySelector("p").innerText = "You are able to get a job!";
+    document.querySelector("h3").innerText = randomNumber + "%";
+    document.querySelector("h2").innerText = "Hey " + userNames + " you got";
   }
   if (randomNumber < 30) {
-    $("p").text("YOOO WATCH OUT, YOUR COOKED NGL JUST CRY! 😂");
-    $("h3").text(`${randomNumber}%`);
-    $("h2").text(`Hey ${userNames}, no luck you got`);
-    $("h4").text("Sorry try again 😖");
+    document.querySelector("p").innerHTML =
+      "YOOO WATCH OUT, YOUR COOKED NGL JUST CRY!";
+    document.querySelector("h3").innerHTML = randomNumber + "%";
+    document.querySelector("h2").innerHTML =
+    document.querySelector("p").innerText =
+      "YOOO WATCH OUT, YOUR COOKED NGL JUST CRY! 😂";
+    document.querySelector("h3").innerText = randomNumber + "%";
+    document.querySelector("h2").innerText =
+      "Hey " + userNames + " no luck you got";
+    document.querySelector("h4").innerHTML = "Sorry try again ;)";
+    document.querySelector("h4").innerText = "Sorry try again 😖";
   }
-})
+}
+});
+jobProbabilityGame();
